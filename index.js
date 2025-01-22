a9ce47442cf2dd5bbd214df7b549a03e60399f17
Geva, Nino Mynel A. <ninomynelageva@iskolarngbayan.edu.gov.ph>
2025-01-22 23:38:25 +0800

    For Deployment

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
+//Just added this
+const http = require('http');
+const server = http.createServer(app);
+
 app.get('/', (req, res) => {
     res.send("Server is running")
 })
@@ -29,10 +33,15 @@ const submitAuditionForm = require('./API/submit')
 //Use API
 app.use("/submit", submitAuditionForm);
 
-// Start the server
-const PORT = 5000;
+// Start the server locally
+//const PORT = 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+//app.listen(PORT, () => {
+//    console.log(`Server is running on http://localhost:${PORT}`);
+//});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+	console.log('Server running on port ${PORT}');
 });
-
